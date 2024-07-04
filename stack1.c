55189ed7802d65505cc2469973721942e26e4c46
kapil <kapil.kumar.baser@gmail.com>
2024-07-04 12:16:31 +0530

    stack with array

---
+++ stack1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "stack.h"
+
+#define STACK_SIZE 100
+
+static int contents[STACK_SIZE];
+static int top = 0;
+
+static void terminate(const char *message)
+{
+    printf("%s\n", message);
+    exit(EXIT_FAILURE);
+}
